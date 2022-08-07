46ab0ddfdbb5c5d5ed55ffdbd2bf7aedde4fbf6f
Patrycja Zalas <mineksoakura@gmail.com>
2022-08-07 23:06:25 +0200

    trials with firebase

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import { db } from './firebase-config';
+import { collection, getDocs } from 'firebase/firestore';
 
 function App() {
-  const [] = useState()
-  return <div className="App" />
+  const [users, setUsers] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+useEffect(() => {
+  const getUsers = async() => {
+    const data = await getDocs(usersCollectionRef);
+    setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
+  }
+  getUsers();
+}, [])
+
+  return <div className="App">
+    {users.map((user) => {
+    return (
+    <div>
+      {" "}
+      <h1>Name: {user.name}</h1>
+      </div>
+      )
+    })}
+      </div>;
 }
 
 export default App;
