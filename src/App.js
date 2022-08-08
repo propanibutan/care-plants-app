6efc90509b146a29b86031f0c28226643bb2a439
Patrycja Zalas <mineksoakura@gmail.com>
2022-08-08 14:16:20 +0200

    config

--- src/App.js
+++ src/App.js
@@ -1,29 +1,20 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import { db } from './firebase-config';
-import { collection, getDocs } from 'firebase/firestore';
+import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+} from 'react-router-dom';
+import NotFound from './components/NotFound';
 
-function App() {
-  const [users, setUsers] = useState([]);
-  const usersCollectionRef = collection(db, "users");
-useEffect(() => {
-  const getUsers = async() => {
-    const data = await getDocs(usersCollectionRef);
-    setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})))
-  }
-  getUsers();
-}, [])
-
-  return <div className="App">
-    {users.map((user) => {
-    return (
-    <div>
-      {" "}
-      <h1>Name: {user.name}</h1>
-      </div>
-      )
-    })}
-      </div>;
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/components/NotFound.js
@@ -0,0 +1,3 @@
+import React from 'react';
+
+export default function NotFound() { return <h1>404,Nothing is here</h1> }
